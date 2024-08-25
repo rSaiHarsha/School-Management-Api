793e3b42e977e79970502a2da12be44c88ca3dfb
Sai Harsha <134918367+rSaiHarsha@users.noreply.github.com>
2024-08-25 18:21:03 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: 5432,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
