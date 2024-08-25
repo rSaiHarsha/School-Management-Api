1d4df2fb959f6ddcfd1485cf2697f7a0013b055f
Sai Harsha <134918367+rSaiHarsha@users.noreply.github.com>
2024-08-25 19:11:10 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const schoolRoutes = require('./routes/schoolRoutes');
 require('dotenv').config();
 require('./config/db'); // Initialize MySQL connection
@@ -8,6 +9,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use('/api/schools', schoolRoutes);
