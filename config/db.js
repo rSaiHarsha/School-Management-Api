fa6adb27fe0579e92d9497989f95964ba96f6dda
Sai Harsha <134918367+rSaiHarsha@users.noreply.github.com>
2024-08-25 18:46:11 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -8,7 +8,25 @@ const db = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+  // SQL query to create a table if it doesn't already exist
+    const createTableQuery = `
+     CREATE TABLE schools (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL
+)`;
 
+   
+db.query(createTableQuery, (err, results) => {
+        if (err) {
+            console.error('Error creating table: ', err);
+            process.exit(1);  // Exit process with failure
+        }
+        console.log('Table created or already exists');
+    });
+});
 db.connect(err => {
     if (err) {
         console.error('Database connection failed: ', err);
@@ -16,5 +34,8 @@ db.connect(err => {
     }
     console.log('Connected to the MySQL database');
 });
+
+
+
 
 module.exports = db;
